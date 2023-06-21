b91ba499491f3c3cd859fadfe788f84406737811
tamyiuchau <tamyiuchau@users.noreply.github.com>
2023-06-21 15:14:28 +0800

    extract out dataloader_procedures

---
+++ src/lib/procedures/dataloaders_procedure.py
@@ -0,0 +1,54 @@
+from lib.modules.dataset.SQLJointsDataset import SQLJointsDataset
+from torch.utils.data.dataloader import DataLoader
+
+
+def create_test_dataloader(BATCH_SIZE):
+    test_dataset = SQLJointsDataset(train=False)
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=2,
+        pin_memory=True,
+        persistent_workers=2,
+    )
+
+    return test_dataloader
+
+
+def create_train_dataloader(BATCH_SIZE):
+    train_dataset = SQLJointsDataset(train=True)
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        num_workers=2,
+        pin_memory=True,
+        persistent_workers=2,
+    )
+
+    return train_dataloader
+
+
+def create_dataloaders(BATCH_SIZE):
+    train_dataloader = create_train_dataloader(BATCH_SIZE)
+    test_dataloader = create_test_dataloader(BATCH_SIZE)
+    return train_dataloader, test_dataloader
+
+
+def create_validation_dataloader(BATCH_SIZE, WITH_QUILT, VALIDATION):
+    if WITH_QUILT:
+        val_dataset = SQLJointsDataset(
+            train=False, mixed=False, all_quilt=True, validation=VALIDATION
+        )
+        quilt_conditions = "all_quilts"
+    else:
+        val_dataset = SQLJointsDataset(
+            train=False, mixed=False, all_quilt=False, validation=VALIDATION
+        )
+        quilt_conditions = "no_quilts"
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=0
+    )
+
+    return val_dataloader
